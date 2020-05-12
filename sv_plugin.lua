6257b66fd75d58f9e3d5aea26fba548ce6f879c4
Dobytchick <48864677+Dobytchick@users.noreply.github.com>
2020-05-13 02:06:09 +0600

    Update sv_plugin.lua

--- sv_plugin.lua
+++ sv_plugin.lua
@@ -1,10 +1,12 @@
-netstream.Hook("givemyitem",function(client,mode,otherclient,item)
+netstream.Hook("givemyitem",function(client,mode,otherclient,item,ccount)
     if !isnumber(mode) then return false end
     if !client:IsAdmin() then return false end
     if mode == 1 then
         client:getChar():getInv():add(item)
     end
     if mode == 2 && otherclient != nil then
-        otherclient:getChar():getInv():add(item)
+        for i=1,ccount do
+            otherclient:getChar():getInv():add(item)
+        end
     end
 end)

2897be42d149e00f5c8a6595e4dc6b64c88ed061
Dobytchick <48864677+Dobytchick@users.noreply.github.com>
2020-05-13 13:49:49 +0600

    Update sv_plugin.lua

--- sv_plugin.lua
+++ sv_plugin.lua
@@ -1,12 +1,21 @@
-netstream.Hook("givemyitem",function(client,mode,otherclient,item,ccount)
+netstream.Hook("givemyitem",function(client,mode,otherclient,item,quantity)
+    print(client,mode,otherclient,item,quantity)
     if !isnumber(mode) then return false end
     if !client:IsAdmin() then return false end
     if mode == 1 then
-        client:getChar():getInv():add(item)
+        client:getChar():getInv():add(item,quantity or 1)
     end
     if mode == 2 && otherclient != nil then
-        for i=1,ccount do
-            otherclient:getChar():getInv():add(item)
+        otherclient:getChar():getInv():add(item,quantity or 1)
+    end
+end)
+
+netstream.Hook("CheckEmpty",function(client,otherclient)
+    if client:IsAdmin() then
+        if otherclient:getChar():getInv():findEmptySlot() != nil then
+            netstream.Start(client,"cl_return",nil,true)
+        else
+            netstream.Start(client,"cl_return",nil,false)
         end
     end
 end)
